d24bbb9863424e8d80f7a75163193012ad991968
DanielApix <danielapix@hotmail.it>
2018-10-26 17:53:20 +0200

    Add files via upload
    Versione due concorrente non funzionante

---
+++ Spire-c/tests/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../sequential_program_main.c"
+
+int main() {
+  printf("Hello world\n");
+}
